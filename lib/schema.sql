9700182030962de5d8648fb9687381d7f5e5cee8
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-28 20:29:00 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,25 @@ CREATE TABLE  Series (
   author INTEGER,
   sub-genre TEXT
 );
+CREATE TABLE Sub-Genre(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+CREATE TABLE Authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+CREATE TABLE Books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series INTEGER
+);
+CREATE TABLE Charcters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author INTEGER,
+  series INTEGER
+);
