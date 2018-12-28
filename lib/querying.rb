85d7613e7efa5152773fdae76cfd62dba3dac36f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-28 20:57:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,13 +17,19 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
   FROM characters
   GROUP BY
-  species
+  species 
   ORDER BY COUNT(species) DESC
   LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+  FROM series 
+  INNER JOIN authors 
+  ON series.author_id = authors.id 
+  INNER JOIN subgenres 
+  ON series_subgenre_id = subgenre.id 
+   "
 end
 
 def select_series_title_with_most_human_characters
